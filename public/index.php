71b533b2409dcace5c911ec6ced104169ada2444
3B232073 <3b232073@gm.student.ncut.edu.tw>
2025-11-19 10:43:29 +0800

    載入Carbo並測試十項程式碼

--- public/index.php
+++ public/index.php
@@ -6,6 +6,8 @@
     use Demo\Hello\Someone;
 	use Monolog\Logger;
     use Monolog\Handler\StreamHandler;
+	use Carbon\Carbon;
+
     $lara = new Lara();
     $vincent = new Someone('Vincent');
     
@@ -22,4 +24,37 @@
 	$log = new Logger('WISD');
 	$log->pushHandler(new StreamHandler(__DIR__ . '/../log/my.log', Logger::DEBUG));
 	$log->info("Monolog 測試成功！");
+	
+	// === Carbon 測試開始 ===
+	
+	// 1. 目前時間
+	$log->info("現在時間：" . Carbon::now());
+
+	// 2. 今天日期
+	$log->info("今天是：" . Carbon::today());
+
+	// 3. 昨天
+	$log->info("昨天是：" . Carbon::yesterday());
+
+	// 4. 明天
+	$log->info("明天是：" . Carbon::tomorrow());
+
+	// 5. +7 天
+	$log->info("七天後：" . Carbon::now()->addDays(7));
+
+	// 6. -3 天
+	$log->info("三天前：" . Carbon::now()->subDays(3));
+
+	// 7. 下週一
+	$log->info("下週一：" . Carbon::now()->next('Monday'));
+
+	// 8. 當前年份
+	$log->info("今年：" . Carbon::now()->year);
+
+	// 9. 當前月份
+	$log->info("這個月：" . Carbon::now()->month);
+
+	// 10. 格式化日期
+	$log->info("格式化：" . Carbon::now()->format('Y-m-d H:i:s'));
+
 ?>
